5a6c199d5445b6e9ee6bca8239acf72f76be6a9b
dankcgi <dan.kiernan@cgi.com>
2026-02-09 12:12:29 +0000

    fix: imports

--- src/handlers/oscillator-management.ts
+++ src/handlers/oscillator-management.ts
@@ -1,5 +1,5 @@
 import type { OscillatorBank } from "../core/oscillator-bank";
-import type { OscillatorControl } from "../components/oscillator-control";
+import type { OscillatorControl } from "../components/molecules/oscillator-control";
 
 export function createOscillatorManager(
   oscillatorBank: OscillatorBank,
